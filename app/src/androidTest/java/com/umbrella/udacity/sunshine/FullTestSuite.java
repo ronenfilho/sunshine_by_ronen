f55504d995aa049d75c002014b8d3830105cc6ad
ronen <ronen.filho@gmail.com>
2015-03-31 17:10:02 -0300

    4.02

---
+++ app/src/androidTest/java/com/umbrella/udacity/sunshine/FullTestSuite.java
@@ -0,0 +1,21 @@
+package com.umbrella.udacity.sunshine;
+
+/**
+ * Created by ronen on 31/03/15.
+ */
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public FullTestSuite() {
+        super();
+    }
+
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+}

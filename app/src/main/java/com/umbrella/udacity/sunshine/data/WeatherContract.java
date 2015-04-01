6e7d6740b442168bc4a484f08bd400ea30378ad0
ronen <ronen.filho@gmail.com>
2015-04-01 15:18:15 -0300

    Ajuste para a Lesson 4.09

--- app/src/main/java/com/umbrella/udacity/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/umbrella/udacity/sunshine/data/WeatherContract.java
@@ -19,7 +19,7 @@ public class WeatherContract {
     // relationship between a domain name and its website.  A convenient string to use for the
     // content authority is the package name for the app, which is guaranteed to be unique on the
     // device.
-    public static final String CONTENT_AUTHORITY = "com.example.android.sunshine.app";
+    public static final String CONTENT_AUTHORITY = "com.umbrella.udacity.sunshine";
 
     // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
     // the content provider.
@@ -53,21 +53,24 @@ public class WeatherContract {
         // The location setting string is what will be sent to openweathermap
         // as the location query.
         public static final String COLUMN_LOCATION_SETTING = "location_setting";
-        public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
         // Human readable location string, provided by the API.  Because for styling,
         // "Mountain View" is more recognizable than 94043.
         public static final String COLUMN_CITY_NAME = "city_name";
         // In order to uniquely pinpoint the location on the map when we launch the
         // map intent, we store the latitude and longitude as returned by openweathermap.
         public static final String COLUMN_COORD_LAT = "coord_lat";
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
         public static final String COLUMN_COORD_LONG = "coord_long";
 
         public static Uri buildLocationUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
+
+
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
 
 
     }
@@ -80,14 +83,12 @@ public class WeatherContract {
         public static final String TABLE_NAME = "weather";
         // Column with the foreign key into the location table.
         public static final String COLUMN_LOC_KEY = "location_id";
-        public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
         // Date, stored as long in milliseconds since the epoch
         public static final String COLUMN_DATE = "date";
         // Weather id as returned by API, to identify the icon to be used
         public static final String COLUMN_WEATHER_ID = "weather_id";
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
         // Short description and long description of the weather, as provided by API.
         // e.g "clear" vs "sky is clear".
         public static final String COLUMN_SHORT_DESC = "short_desc";
@@ -96,6 +97,8 @@ public class WeatherContract {
         public static final String COLUMN_MAX_TEMP = "max";
         // Humidity is stored as a float representing percentage
         public static final String COLUMN_HUMIDITY = "humidity";
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
         // Humidity is stored as a float representing percentage
         public static final String COLUMN_PRESSURE = "pressure";
         // Windspeed is stored as a float representing windspeed  mph

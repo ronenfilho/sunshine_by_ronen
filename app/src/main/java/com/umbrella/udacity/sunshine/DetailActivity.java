3cea54d95b19dad0455c22cb73178cd5dbcda4bd
ronen <ronen.filho@gmail.com>
2015-04-12 02:27:50 -0300

    Ajuste para a Lesson 4.20

--- app/src/main/java/com/umbrella/udacity/sunshine/DetailActivity.java
+++ app/src/main/java/com/umbrella/udacity/sunshine/DetailActivity.java
@@ -1,10 +1,10 @@
 package com.umbrella.udacity.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -69,17 +68,23 @@ public class DetailActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
             // The detail Activity called via intent.  Inspect the intent for forecast data.
             Intent intent = getActivity().getIntent();
-            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
                 mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-                ((TextView)rootView.findViewById(R.id.detail_text))
+            }
+
+            if (null != mForecastStr) {
+                ((TextView) rootView.findViewById(R.id.detail_text))
                         .setText(mForecastStr);
             }
+
             return rootView;
         }
+
         @Override
         public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
             // Inflate the menu; this adds items to the action bar if it is present.
@@ -110,6 +115,4 @@ public class DetailActivity extends ActionBarActivity {
             return shareIntent;
         }
     }
-
 }
-
